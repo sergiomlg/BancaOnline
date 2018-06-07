459d9eee0210b1fcc0eb1f27e07f4caa1d0d427e
EzequielRodriguez <ezequiel.rm.97gmail.com>
2018-06-07 11:50:14 +0200

    Creacion listadoClientes creacion editarCliente creacion sus correspondientes beans

--- BancaOnline-war/src/java/bancaonline/bean/bOnlineBean.java
+++ BancaOnline-war/src/java/bancaonline/bean/bOnlineBean.java
@@ -5,22 +5,63 @@
  */
 package bancaonline.bean;
 
+import bancaonline.ejb.UsuarioFacade;
+import bancaonline.entity.Usuario;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 
 /**
  *
  * @author EzequielRodriguez
  */
-@Named(value = "bOnlineBean")
+@Named(value = "bOnlineBean") //Bean de listado Clientes
 @SessionScoped
 public class bOnlineBean implements Serializable {
 
-    /**
-     * Creates a new instance of bOnlineBean
-     */
+   @EJB private UsuarioFacade userFacade;
+   
+   private List<Usuario> listaClientes;
+   
+   private String idUsuarioSeleccionado;
+   
+    
     public bOnlineBean() {
+    }
+
+    public List<Usuario> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(List<Usuario> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public String getIdUsuarioSeleccionado() {
+        return idUsuarioSeleccionado;
+    }
+
+    public void setIdUsuarioSeleccionado(String idUsuarioSeleccionado) {
+        this.idUsuarioSeleccionado = idUsuarioSeleccionado;
+    }
+    
+    
+    
+    @PostConstruct
+    public void init(){
+        listaClientes = this.userFacade.findAll();
+    }
+    
+    public String doEditar(String idUsuario){
+        this.idUsuarioSeleccionado = idUsuario;
+        return "editarCliente.xhtml";
+    }
+    
+    public String doEliminar(String idUsuario){
+        return "listaClientes.xhtml";
     }
     
 }
---
+++ BancaOnline-war/src/java/bancaonline/bean/bOnlineEditar.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancaonline.bean;
+
+import bancaonline.ejb.UsuarioFacade;
+import bancaonline.entity.Usuario;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author EzequielRodriguez
+ */
+@Named(value = "bOnlineEditar")
+@RequestScoped
+public class bOnlineEditar {
+
+    @Inject private bOnlineBean bOnlineBean;
+    
+    @EJB private UsuarioFacade userFacade;
+    
+    private Usuario user;
+    
+    public bOnlineEditar() {
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+
+    
+    public bOnlineBean getbOnlineBean() {
+        return bOnlineBean;
+    }
+
+    public void setbOnlineBean(bOnlineBean bOnlineBean) {
+        this.bOnlineBean = bOnlineBean;
+    }
+    
+    @PostConstruct
+    public void init(){
+        this.user = this.userFacade.find(this.bOnlineBean.getIdUsuarioSeleccionado());
+    }
+    
+    public String doGuardar(){
+        this.userFacade.edit(user);
+        return "listadoClientes.xhtml";
+    }
+    
+    
+}
